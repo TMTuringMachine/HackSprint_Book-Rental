2e39d1fb3ed9606c5e338f30d4637897497e5df2
Rewa Wader <raw.rewa10@gmail.com>
2022-10-14 23:01:42 +0530

    rating apis + order merge conflicts

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -4,9 +4,9 @@ const {
     checkout,
     createOrder,
     payment,
-    // addToCart,
-    // removeFromCart,
-    // getCart
+    addToCart,
+    removeFromCart,
+    getCart
 } = require("../controllers/OrderController");
 
 const { isLoggedIn } = require("../middlewares/isLoggedIn");
@@ -14,9 +14,9 @@ const { isLoggedIn } = require("../middlewares/isLoggedIn");
 router.post("/create",isLoggedIn, createOrder);
 router.post("/checkout",isLoggedIn, checkout);
 router.post("/pay",isLoggedIn,payment)
-// router.post('/addToCart',isLoggedIn,addToCart)
-// router.post('/removeFromCart',isLoggedIn,removeFromCart)
-// router.get('/viewCart',isLoggedIn,getCart)
+router.post('/addToCart',isLoggedIn,addToCart)
+router.post('/removeFromCart',isLoggedIn,removeFromCart)
+router.get('/viewCart',isLoggedIn,getCart)
 
 
 
