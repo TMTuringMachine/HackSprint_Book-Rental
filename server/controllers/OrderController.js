5923f7614452e133a55f2c595e200cd83a803ee3
Vedant Gokhale <gokhalevedant06@gmail.com>
2022-10-15 01:49:10 +0530

    Added Order Summary API

--- server/controllers/OrderController.js
+++ server/controllers/OrderController.js
@@ -5,6 +5,7 @@ const User = require('../models/UserSchema');
 const Order = require('../models/OrderSchema');
 const Book = require('../models/BookSchema');
 
+
 const createOrder = async (req, res) => {
   var instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
@@ -115,36 +116,66 @@ const getCart = async (req, res) => {
 };
 
 const checkout = async (req, res) => {
-  const user = req.user;
-  let address11;
-  if (user) {
-    console.log('okay user is verified');
-    let cart = user.cart.items;
-    let books = cart?.map((e) => e.id);
-    console.log('books here', books);
-    var totalPrice = cart.total;
-    var { address, city, state, pincode } = req.body;
-    // let addressobj=await user.Addresses
-    console.log('ADRESSsf ', user.Addresses);
-    let address1 = user.Addresses.filter(
-      (e) =>
-        e.address == address &&
-        e.city == city &&
-        e.state == state &&
-        e.pincode == pincode
-    );
-    console.log('now', address);
-    if (address1.length !== 0) {
-      console.log('is there');
-      address11 = { address, city, state, pincode };
-    } else {
-      console.log('else here');
-      let prev = user.Addresses;
-      prev.push({ address, city, state, pincode });
-      address11 = { address, city, state, pincode };
-      const userupdate = await User.findByIdAndUpdate(user.id, {
-        Addresses: prev,
-      });
+    const user = req.user
+    let address11;
+    if (user) {
+      console.log("okay user is verified");
+      let cart = user.cart.items;
+      console.log(cart,"CART")
+      let books = cart?.map((e) => e);
+      console.log("books here", books);
+      var totalPrice = cart.total;
+      var { address, city, state, pincode } = req.body;
+      // let addressobj=await user.Addresses
+      console.log("ADRESSsf ", user.Addresses);
+      let address1 = user.Addresses.filter(
+        (e) =>
+          e.address == address &&
+          e.city == city &&
+          e.state == state &&
+          e.pincode == pincode
+      );
+      console.log("now", address);
+      if (address1.length !== 0) {
+        console.log("is there");
+        address11 = { address, city, state, pincode };
+      } else {
+        console.log("else here");
+        let prev = user.Addresses;
+        prev.push({ address, city, state, pincode });
+        address11 = { address, city, state, pincode };
+        const userupdate = await User.findByIdAndUpdate(user.id, {
+          Addresses: prev,
+        });
+      }
+      try {
+        console.log(address11);
+
+        const order = new Order({
+          books,
+          totalPrice,
+          Address: address11,
+          isOutForDelivery: false,
+          isDelivered: false,
+          isReturned: false,
+        });
+        const ordersaved = await order.save();
+        if (ordersaved) {
+          let currrentals = user.rentals;
+          currrentals.push({ isActive: true, order: ordersaved })
+          console.log("HERE",user)
+          const usr = await User.findByIdAndUpdate(user.id,{ rentals:currrentals,cart:{
+            items:[],
+            total:0
+          }})
+
+        if(usr) res.status(200).send({ message: "order checked out" ,orderID:ordersaved._id });
+        }
+      } catch (error) {
+        res.status(403).send({ message: "error occured" });
+
+        console.log("Error occured",error);
+      }
     }
     try {
       console.log(address11);
@@ -227,6 +258,15 @@ const getAllRentals = async (req, res) => {
     res.status(500).send({ message: 'some error occurred' });
   }
 };
+const orderSummary = async(req,res)=>{
+    const {orderID} = req.body;
+    const orderDetails = await Order.findById(orderID).populate("books")
+    if(orderDetails) res.status(200).send({message:"Order Summary",orderDetails})
+    else{
+        res.status(400).send({message:"Error occurred"})
+    }
+}   
+
 
 module.exports = {
   createOrder,
@@ -237,4 +277,5 @@ module.exports = {
   checkout,
   getRentals,
   getAllRentals,
+  orderSummary
 };
